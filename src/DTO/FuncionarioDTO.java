27b1941c4b4661a1e59299d3127ccb365931e33f
Jonathas Ricardo <71514703+JohnRSS@users.noreply.github.com>
2022-06-12 10:22:06 -0300

    Add files via upload

---
+++ src/DTO/FuncionarioDTO.java
@@ -0,0 +1,45 @@
+
+package DTO;
+
+
+public class FuncionarioDTO {
+    private String nome_funcionario, endereco_funcionario, cargo_funcionario;
+    private int id_funcionario;
+
+    public String getNome_funcionario() {
+        return nome_funcionario;
+    }
+
+    public void setNome_funcionario(String nome_funcionario) {
+        this.nome_funcionario = nome_funcionario;
+    }
+
+    public String getEndereco_funcionario() {
+        return endereco_funcionario;
+    }
+
+    public void setEndereco_funcionario(String endereco_funcionario) {
+        this.endereco_funcionario = endereco_funcionario;
+    }
+
+    public String getCargo_funcionario() {
+        return cargo_funcionario;
+    }
+
+    public void setCargo_funcionario(String cargo_funcionario) {
+        this.cargo_funcionario = cargo_funcionario;
+    }
+
+    public int getId_funcionario() {
+        return id_funcionario;
+    }
+
+    public void setId_funcionario(int id_funcionario) {
+        this.id_funcionario = id_funcionario;
+    }
+
+    public void setId_funcionario(String id_funcionario) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
